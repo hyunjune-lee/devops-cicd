20d2244443faaf92d2d7420626da7aa3383bcb91
Hyunjune Lee <44167177+hyunjune-lee@users.noreply.github.com>
2021-07-09 02:46:28 +0900

    compose 연동 에러만 남겨둠
    docker build한 이미지로 compose하면 안된다. 이때 app 이미지 문제가 있어 보이는게 app 컨테이너를 끄고 db컨테이너와 python main.py로는 연결이 된다.

--- flask-app/db_config.py
+++ flask-app/db_config.py
@@ -1,13 +1,12 @@
 from app import app
 from flaskext.mysql import MySQL
-import os
 
 mysql = MySQL()
 
 # MySQL configurations
 app.config["MYSQL_DATABASE_USER"] = "root"
 app.config["MYSQL_DATABASE_PASSWORD"] = "mysecret"
-app.config["MYSQL_DATABASE"] = "daily_quiz"
+app.config["MYSQL_DATABASE_DB"] = "daily_quiz"
 app.config["MYSQL_DATABASE_HOST"] = "localhost"
 
 mysql.init_app(app)
